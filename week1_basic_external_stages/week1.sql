b6e378a13c44ff7e3860487338f4954313846599
田代 学 / Gaku Tashiro <g.tashiro@churadata.okinawa>
2024-03-28 23:45:08 +0900

    new challenge!

---
+++ week1_basic_external_stages/week1.sql
@@ -0,0 +1,79 @@
+-- データベースを作ります
+create database frosty_friday;
+
+-- SYSADMINの方が妥当かも
+use role accountadmin;
+
+create or replace stag week_ext_stage
+  URL='s3://frostyfridaychallenges/challenge_1/';
+
+list @week_ext_stage;
+
+select 
+    $1
+    -- , $2
+    -- , $3
+    , metadata$filename 
+    , metadata$file_row_number
+from 
+    @week_ext_stage
+;
+
+create or replace table week1_table (
+    result text
+    , filename text
+    , file_row_number number
+)
+;
+desc table week1_table;
+
+-- file formatの作成
+-- CSV
+-- ヘッダは一行目
+-- 文字列「NULL」と「null」をnullへ
+-- 空白のみはnull
+create or replace file format week1_csv_format
+  type = CSV
+  skip_header = 1
+  null_if = ('NULL', 'null')
+  empty_field_as_null = true
+;
+
+-- データの投入
+copy into week1_table ( result, filename, file_row_number)
+from 
+    ( 
+        select 
+            $1 
+            , metadata$filename 
+            , metadata$file_row_number
+        from 
+            @week_ext_stage
+    )
+FILE_FORMAT = (FORMAT_NAME = 'week1_csv_format');
+;
+
+-- file_format を使わない場合
+-- select * from week1_table where result != 'result' and result != 'NULL';
+
+-- file_format を使う場合
+select * from week1_table ;
+
+with source as (
+    select 
+        result
+        , filename
+        , file_row_number
+    from 
+        week1_table 
+    where
+        result is not null
+)
+
+, final as (
+    select 
+        listagg(result,' ') WITHIN GROUP (ORDER BY filename,  file_row_number )  as answer
+    from source
+)
+-- select * from source;
+select * from final;
